e4cba1d770a522ddee77148d346b4c42ecb44bcb
Diadkov Viktor <v.k.diadkov@gmail.com>
2025-11-14 11:55:10 +0200

    Added doxygen docs to the project

--- include/WebServer.h
+++ include/WebServer.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "TcpListener.h"
 
-struct ServerConfig 
+struct ServerConfig
 {
 	std::string host = "127.0.0.1";
 	int         port = 8080;
 	// std::string root = "wwwroot";
 	// std::string index = "index.html";
 };
+
+/**
+ * @brief This is main class WebServer which has all basics
+ * functionality
+ *
+ *
+*/
 
 class WebServer : public TcpListener
 {
@@ -23,14 +30,34 @@ public:
 		: TcpListener(ipAddress, port) {
 	}
 protected:
-
+	/**
+	 * @brief Loads and parses the details created by user.
+	 * (see folder 'resources' for details)
+	 * @return std::pair<std::string, int>
+	 * Return a pair of <host_name, port_value>
+	 * - std::string : host name	(e.g. 127.0.0.1)
+	 * - int  		 : port number	(e.g. 8080)
+	 */
 	static std::pair<std::string, int> loadConfigFile(void);
-	// Handler for client connections
+	/**
+	 * @brief Handler for client connections
+	 * @param clientSocket The socket descriptor of the connected client
+	 */
 	virtual void onClientConnected(int clientSocket);
-
-	// Handler for client disconnections
+	/**
+	 * @brief Handler for client disconnections
+	 *
+	 * @param clientSocket The socket descriptor of the client that disconnected
+	 */
 	virtual void onClientDisconnected(int clientSocket);
-
-	// Handler for when a message is received from the client
+	/**
+	 * @brief  Handler for when a message is received from the client.
+	 * @param clientSocket - The socket descriptor of the client.
+	 * @param msg A view of the received message. The server uses recv()  
+	 * to read raw bytes into a char buffer, and std::string_view 
+	 * provides a lightweight, non-owning view of that buffer.
+	 * @param length The number of bytes received from recv(), representing 
+	 * the actual message length.
+	 */
 	virtual void onMessageReceived(int clientSocket, std::string_view msg, int length);
 };
